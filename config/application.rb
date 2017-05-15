403437a94056d86e369d8d2c52a40bfbbc2829cc
Joe Spampinato <jcspampinato@gmail.com>
2017-05-15 14:34:02 -0400

    changed config to prevent database attempted connection when precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module PopEFX
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # do not attempt to connect to database when precompiling assets
+    config.assets.initialize_on_precompile = false
+
     config.before_initialize do
         # setup the Searchkick client
         #
